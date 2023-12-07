8d81a1a4905674235184c66951ef069c0b573ead
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2023-12-08 00:21:07 +0600

    added mobile compitable navbar

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    title = 'home'
+    title = 'Home'
     context = {'title': title}
     
     return render(request, 'main/index.html', context)
