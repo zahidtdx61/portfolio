93129a2c99987de19623b1f67a7246e4e517bd48
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-10-18 23:19:49 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <div>Home</div>
+    </>
+  );
+};
+
+export default Home;
