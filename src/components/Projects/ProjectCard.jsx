fd5dc7d20fe1e48d0826fbbbb3a7ef680d42e3ee
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-10-20 12:30:56 +0600

    fixed button position

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -2,7 +2,7 @@ const ProjectCard = ({ SiteDetails }) => {
   const { siteName, siteInfo, image, techStack, linkDatas } = SiteDetails;
 
   return (
-    <div className="w-full md:w-[70%} lg:w-[45%] bg-zinc-50 hover:bg-slate-100 flex flex-col shadow-md">
+    <div className="w-full md:w-[70%} lg:w-[45%] p-4 bg-zinc-100 hover:bg-slate-100 flex flex-col shadow-md">
       <div className="w-full p-2 h-96 mx-auto">
         <img
           src={image}
@@ -10,6 +10,7 @@ const ProjectCard = ({ SiteDetails }) => {
           className="h-full w-full object-cover object-center rounded-md"
         />
       </div>
+
       <div className="mt-4 px-2 flex flex-wrap gap-4">
         {techStack.map((tech, index) => (
           <div
@@ -23,20 +24,22 @@ const ProjectCard = ({ SiteDetails }) => {
       <div className="p-4">
         <h2 className="text-xl font-bold">{siteName}</h2>
         <p className="text-gray-600 mt-2">{siteInfo}</p>
-        <div className="flex flex-wrap gap-4">
-          {linkDatas.map((linkData, index) => (
-            <div className="mt-4" key={index}>
-              <a
-                href={linkData.link}
-                target="_blank"
-                className="bg-slate-500 text-white py-2 px-4 rounded-md hover:bg-slate-600"
-              >
-                {linkData.text}
-              </a>
-            </div>
-          ))}
-        </div>
       </div>
+
+      <div className="flex flex-wrap gap-4">
+        {linkDatas.map((linkData, index) => (
+          <div className="mt-4" key={index}>
+            <a
+              href={linkData.link}
+              target="_blank"
+              className="bg-slate-500 text-white py-2 px-4 rounded-md hover:bg-slate-600"
+            >
+              {linkData.text}
+            </a>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 };
